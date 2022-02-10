91351701cceb735cea5796f92077e72b940e1077
jonathangoulding <jonathangoulding21@gmail.com>
2022-02-10 08:33:49 +0000

    chore: postgres db start script for docker

--- start-database.sh
+++ start-database.sh
@@ -1,8 +1,10 @@
+docker stop postgres    
+
 docker run -d \
     --name postgres \
     --rm \
     -e POSTGRES_USER=postgres \
-    -e POSTGRES_PASSWORD=super-string-password \
+    -e POSTGRES_PASSWORD=password \
     -e PGDATA=/var/lib/postgresql/data/pgdata \
     -v /tmp:/var/lib/postgresql/data \
     -p 5432:5432 \
